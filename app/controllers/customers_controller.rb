2ee517011f1c09bde0753c6a1ebfd974b2db23ee
Hannah <hannahvanseeters@gmail.com>
2018-11-05 14:49:23 -0800

    filled in movie controller index, show create, customer controller index, added rabl gem and added forgery protection to controllers

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,16 @@
 class CustomersController < ApplicationController
+  protect_from_forgery with: :null_session
+
+  def index
+    @customers = Customer.all
+
+    render json: @customers
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :registered_at, :address, :city,
+      :state, :postal_code, :phone)
+  end
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,41 @@
 class MoviesController < ApplicationController
+  protect_from_forgery with: :null_session
+
+  def index
+    @movies = Movie.all
+
+    render json: @movies
+  end
+
+  def show
+    @movie = Movie.find_by(id: params[:id])
+
+    if @movie.nil?
+      render json: { ok: false, message: 'not found' }, status: :not_found
+    else
+      render json: @movie(except: [:created_at, :updated_at]), status: :ok
+    end
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      render json: { ok: true,
+      movie = @movie.as_json(except: [:created_at, :updated_at])}
+    else
+      render json: {
+        ok: false,
+        message: @movie.errors.messages
+      }, status: :bad_request
+    end 
+
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :overview, :release_date, :inventory)
+  end
+
 end
